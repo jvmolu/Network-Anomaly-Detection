c5accfcdf1a66cac51c20795e28f750db9eff223
Jvmolu <anmol.singhal50@gmail.com>
2024-04-07 01:55:53 +0530

    Complete

--- synthetic_data_generator.py
+++ synthetic_data_generator.py
@@ -15,11 +15,12 @@ def generate_data(num_nodes, average_bytes_per_edge, count, multiplier=3):
         if np.random.uniform(0, 1) < 0.95:
             for i in range(int(num_edges)):
                 sample_noise = np.random.normal(0, 1)
+                sample_noise = min(sample_noise, 1) # Cap the noise to 1
                 row_data.append(max(average_bytes_per_edge + average_bytes_per_edge*sample_noise, 0))
         else:
             # Anomalies
             for i in range(int(num_edges)):
-                row_data.append(3*average_bytes_per_edge + np.random.uniform(1, 2)*multiplier)
+                row_data.append(3*average_bytes_per_edge + np.random.uniform(3, 4)*multiplier)
 
         data.append(row_data)
         
